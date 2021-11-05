dfe7243003d78d64d3eae907566ae1edabd1fc05
AmiraBelkis <ia_feliachi@esi.dz>
2021-11-05 09:29:05 +0100

    the calculation are wrong

--- script.js
+++ script.js
@@ -9,26 +9,57 @@ const LA = document.getElementById('LA'),
     distance = document.getElementById('D'),
     direction = document.getElementById('dir');
 
+
 function calculat() {
+    let lamdaA, lamdaB, gammaB, gammaA;
     if (document.getElementById('dd').checked) {
-        let gammaA = document.getElementById('gammaA').value * 3.14 / 180,
-            gammaB = document.getElementById('gammaB').value * 3.14 / 180,
-            lamdaA = document.getElementById('lamdaA').value * 3.14 / 180,
-            lamdaB = document.getElementById('lamdaB').value * 3.14 / 180;
+        gammaA = document.getElementById('gammaA').value * 3.14 / 180
+        gammaB = document.getElementById('gammaB').value * 3.14 / 180
+        lamdaA = document.getElementById('lamdaA').value * 3.14 / 180
+        lamdaB = document.getElementById('lamdaB').value * 3.14 / 180
     } else {
 
-        let gammaA = document.getElementById('gammaA').value * 3.14 / 180,
-            gammaB = document.getElementById('gammaB').value * 3.14 / 180,
-            lamdaA = document.getElementById('lamdaA').value * 3.14 / 180,
-            lamdaB = document.getElementById('lamdaB').value * 3.14 / 180;
-    }
-    switch (document.getElementById('method')) {
-        case (1):
+        lamdaA = ConvertDMS_DD(document.getElementById('lamdaA_D').value,
+            document.getElementById('lamdaA_M').value,
+            document.getElementById('lamdaA_S').value) * 3.14 / 180
+        lamdaB = ConvertDMS_DD(document.getElementById('lamdaB_D').value,
+            document.getElementById('lamdaB_M').value,
+            document.getElementById('lamdaB_S').value) * 3.14 / 180
+        gammaA = ConvertDMS_DD(document.getElementById('gammaA_D').value,
+            document.getElementById('gammaA_M').value,
+            document.getElementById('gammaA_S').value) * 3.14 / 180
+        gammaB = ConvertDMS_DD(document.getElementById('gammaB_D').value,
+            document.getElementById('gammaB_M').value,
+            document.getElementById('gammaB_S').value) * 3.14 / 180
 
+
+    }
+    let d = 0;
+    switch (document.getElementById('method').value) {
+        case ('1'): //Pythagore
+            const k = 1.852 * 60;
+            let x = (lamdaB - lamdaA) * Math.cos((gammaA + gammaB) / 2);
+            let y = gammaB - gammaA;
+            let z = Math.sqrt(x * x + y * y);
+            d = z * k;
+            distance.value = Math.round(d * 10000) / 10000 + " Km";
             break;
-        case (2):
+        case ('2'): //sinus
+            const RT = 6378.138;
+            d = RT * Math.acos(Math.sin(lamdaA) * Math.sin(lamdaB) + Math.cos(lamdaA) * Math.cos(lamdaB) * Math.cos(gammaB - gammaA));
+            distance.value = Math.round(d * 10000) / 10000 + " Km";
             break;
-        case (3):
+        case ('3'): //Haversine
+
+            const k2 = 6378.138;
+            x = Math.sin((gammaB - gammaA) / 2) * Math.sin((gammaB - gammaA) / 2);
+            x += Math.cos(gammaA) * Math.cos(gammaB) * Math.sin((lamdaB - lamdaA) / 2) * Math.sin((lamdaB - lamdaA) / 2);
+            y = 2 * Math.atan(Math.sqrt(x) / Math.sqrt(1 - x));
+            d = k2 * y;
+            distance.value = Math.round(d * 10000) / 10000 + " Km";
+            break;
+        default:
+            console.log('no where');
             break;
     }
 }
@@ -56,4 +87,17 @@ function DD_DMS(n) {
             GB.style.display = 'none';
             break;
     }
+}
+
+function ConvertDMS_DD(a, b, c) {
+    let x;
+    let y = (b + c / 60) / 60;
+    if (a > 0) {
+        x = a + y;
+    } else {
+        let u = a * (-1);
+        let f = u + y;
+        x = f * (-1);
+    }
+    return x;
 }
